38cd2121874a078fe33534228c6b6224fdd95ff1
mariannebarbin-png <mariannebarbin@gmail.com>
2025-10-22 23:14:56 +0800

    editedagain

--- src/script.js
+++ src/script.js
@@ -27,9 +27,12 @@ scene.add(house)
 
 //WallTextures
 const bricksColorTexture = textureLoader.load('./textures/bricks/color.jpg')
-const bricksAmbientOcclusionTexture = textureLoader.load('./textures/bricks/ambientOcclusion.jpg')
-const bricksNormalTexture = textureLoader.load('./textures/bricks/normal.jpg')
-const bricksRoughnessTexture = textureLoader.load('./textures/bricks/roughness.jpg')
+const bricksAmbientOcclusionTexture =
+textureLoader.load('./textures/bricks/ambientOcclusion.jpg')
+const bricksNormalTexture =
+textureLoader.load('./textures/bricks/normal.jpg')
+const bricksRoughnessTexture =
+textureLoader.load('./textures/bricks/roughness.jpg')
 
 // Walls
 const walls = new THREE.Mesh(
@@ -54,13 +57,16 @@ roof.rotation.y = Math.PI / 4
 house.add(roof)
 
 //DoorTextures
-const doorColorTexture = textureLoader.load('./textures/door/roughness.jpg')
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('./textures/door/alpha.jpg')
-const doorAmbientOcclusionTexture = textureLoader.load('./textures/door/ambientOcclusion.jpg')
+const doorAmbientOcclusionTexture =
+textureLoader.load('./textures/door/ambientOcclusion.jpg')
 const doorHeightTexture = textureLoader.load('./textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('./textures/door/normal.jpg')
-const doorMetalnessTexture = textureLoader.load('./textures/door/metalness.jpg')
-const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
+const doorMetalnessTexture =
+textureLoader.load('./textures/door/metalness.jpg')
+const doorRoughnessTexture =
+textureLoader.load('./textures/door/roughness.jpg')
 
 //Door
 const door = new THREE.Mesh(
